da84678840707c86c9d9b674097dfe6b68ac7c06
Supriya334 <124588951+Supriya334@users.noreply.github.com>
2024-08-11 23:34:12 +0530

    login page

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let searchBtn = document.querySelector("#search-btn");
 let searchBar = document.querySelector(".search-bar-container");
 
+
+window.onscroll=()=>{
+    searchBtn.classList.remove('bx-x-circle');
+    searchBar.classList.remove('active');
+}
+
+
 searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('bx-x-circle');
     searchBar.classList.toggle('active');
