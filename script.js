edc1fb66d1ea4a5c67244ad02eab7cbca3876c59
Supriya334 <124588951+Supriya334@users.noreply.github.com>
2024-08-13 13:28:01 +0530

    responsiveness

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 let searchBtn = document.querySelector("#search-btn");
 let searchBar = document.querySelector(".search-bar-container");
-
 let formBtn = document.querySelector("#login-btn");
 let loginForm= document.querySelector(".login-form-container");
-
 let formClose=document.querySelector("#form-close");
+let menu=document.querySelector("#menu-bar");
+let navbar=document.querySelector(".navbar")
+
+
 
 window.onscroll=()=>{
     searchBtn.classList.remove('bx-x-circle');
     searchBar.classList.remove('active');
     loginForm.classList.remove('active');
 };
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('bx-x-circle');
+    navbar.classList.toggle('active');
+});
+
+
 
 searchBtn.addEventListener('click', () => {
     searchBtn.classList.toggle('bx-x-circle');
