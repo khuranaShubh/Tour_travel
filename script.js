dde5fbffa94256d9a84ffe36771eac5afc70dbbe
Supriya Singh <124588951+Supriya334@users.noreply.github.com>
2024-08-21 13:46:36 +0530

    updated code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let searchBtn = document.querySelector("#search-btn");
 let searchBar = document.querySelector(".search-bar-container");
 let formBtn = document.querySelector("#login-btn");
-let loginForm= document.querySelector(".login-form-container");
-let formClose=document.querySelector("#form-close");
-let menu=document.querySelector("#menu-bar");
-let navbar=document.querySelector(".navbar")
- 
+let loginForm = document.querySelector(".login-form-container");
+let formClose = document.querySelector("#form-close");
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar")
+let videoBtn = document.querySelectorAll('.vid-btn')
 
 
-window.onscroll=()=>{
+
+window.onscroll = () => {
     searchBtn.classList.remove('bx-x-circle');
     searchBar.classList.remove('active');
     loginForm.classList.remove('active');
@@ -34,3 +35,15 @@ formBtn.addEventListener('click', () => {
 formClose.addEventListener('click', () => {
     loginForm.classList.remove('active');
 });
+
+// vdo part home
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src=src;
+
+    })
+})
